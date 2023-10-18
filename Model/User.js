e75f96215f092464641a975d691717c5de9880c8
Lameed88 <moradeyor88@gmail.com>
2023-10-18 15:27:19 +0100

    first commit

---
+++ Model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    roles: {
+    User: {
+        type: Number,
+        default: 4000
+    },
+    Editor: Number,
+    Admin: Number
+
+    },
+// role:{
+  //   type:String,
+  //   default:"User"
+  //     Admin,Editor
+  // }
+
+    password: {
+        type:String,
+        required: true
+    }, 
+    refreshToken: String
+
+})
+
+const User = mongoose.model("User", userSchema)
+module.exports = User
