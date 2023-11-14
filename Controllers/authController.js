2da14a0fc5e045e08209154772588c7302cd64e4
Lameed88 <moradeyor88@gmail.com>
2023-11-14 12:34:06 +0100

    update

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -13,7 +13,7 @@ const User = require('../Model/User')
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-      const roles = Object.values(foundUser.roles)
+      const roles = Object.values(foundUser.roles).filter(Boolean)
         // create JWTs
         const accessToken = jwt.sign(
             { 
@@ -35,7 +35,7 @@ const User = require('../Model/User')
       const result = await foundUser.save()
       console.log(result);
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
-        res.json({ accessToken });
+        res.json({roles, accessToken });
     } else {
         res.sendStatus(401);
     }
